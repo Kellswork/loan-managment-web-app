b00e89802455eb2192b2353beb5dac79bdbd7d9d
Kellswork <kellogbonna@gmail.com>
2024-05-05 22:27:04 +0100

    add dashboard layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./dashboard.scss";
+import Nav from "../components/navigation/navigation";
+import Sidebar from "../components/sidebar/sidebar";
+import "./dashboard.scss"
+
+export const metadata: Metadata = {
+  title: "lendsqr Dashboard",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+    <Nav/>
+      <div className="dashboard-container">
+        <Sidebar />
+        <main>{children}</main>
+      </div>
+    </>
+  );
+}
