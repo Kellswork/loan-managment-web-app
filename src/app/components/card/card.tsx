9b851cf532da8a48d37b69a0efc15f35db0327b5
Kellswork <kellogbonna@gmail.com>
2024-05-06 12:12:28 +0100

    update card icons and texts

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -18,36 +18,36 @@ const Card = () => {
       </div>
       <div className="card-item">
         <Image
-          src="/users-ellipse.svg"
+          src="/active-users.svg"
           alt="users ellipse icon svg"
           width={40}
           height={40}
           priority
         />
-        <p>users</p>
+        <p>active users</p>
         <p>2,453</p>
       </div>
       <div className="card-item">
         <Image
-          src="/users-ellipse.svg"
-          alt="users ellipse icon svg"
+          src="/users-with-loan.svg"
+          alt="users with loan svg"
           width={40}
           height={40}
           priority
         />
-        <p>users</p>
-        <p>2,453</p>
+        <p>users with loans</p>
+        <p>12,453</p>
       </div>
       <div className="card-item">
         <Image
-          src="/users-ellipse.svg"
-          alt="users ellipse icon svg"
+          src="/users-with-savings.svg"
+          alt="users ewith savings svg"
           width={40}
           height={40}
           priority
         />
-        <p>users</p>
-        <p>2,453</p>
+        <p>users with savings</p>
+        <p>102,453</p>
       </div>
     </div>
   );
