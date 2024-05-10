81666d67bfb465c54a660c07a0279f38bd94c942
Kellswork <kellogbonna@gmail.com>
2024-05-10 15:44:49 +0100

    update state management error

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,11 +1,14 @@
-import { UserDataProps, UserDetailsDataProps } from "@/utils/userDetails";
+import { UserDataProps } from "@/utils/userDetails";
 
 export const fetchUsersData = async () => {
   try {
     const response = await fetch(
-      "https://run.mocky.io/v3/4a5a47e2-3680-4181-a1d8-b6837f9815fb",
+      "https://run.mocky.io/v3/022c5272-7dc8-44ac-aecd-8c52eaec579b",
       { next: { revalidate: 3600 } }
     );
+       if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+    }
     const responseData = (await response.json()) as UserDataProps[];
 
     const users = responseData
@@ -17,12 +20,12 @@ export const fetchUsersData = async () => {
 
     return users;
   } catch (error) {
-    let err: string | undefined;
-    if (error instanceof TypeError) {
-      err = error.message;
-    }
-    console.error("Error fetching data:", err);
+    console.error("Error fetching data:", error);
     return `There was a problem fetching the data`;
   }
 };
 
+
+
+// https://run.mocky.io/v3/4a5a47e2-3680-4181-a1d8-b6837f9815fb
+//use for testing error response 
